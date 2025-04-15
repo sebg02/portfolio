2c18ec6f1be44c75c734a6f40137521e06f2d588
SEB <sebasgr00@gmail.com>
2025-04-14 20:05:46 -0500

    add content

--- src/context/english.js
+++ src/context/english.js
@@ -1,89 +1,110 @@
 export const en = {
-  menus: {
-    home: "Home",
-    about: "About",
-    skills: "Skills",
-    studies: "Studies",
-    experience: "Experience",
-    projects: "Projects",
-    contact: "Contact",
-    language: "Language",
-  },
-  welcome: {
-    title: "Hey there,",
-    p1: "My name is Sebastián",
-    p2: "I am a Software Developer",
-    b1: "Get in touch",
-    b2: "Download Resume",
-  },
-  about: {
-    title: "About me",
-    description:
-      "My name is Sebastian and I am a passionate software developer dedicated to building robust and efficient solutions. I have worked with technologies such as Django, Python, JavaScript, React, HTML, and CSS, always striving to optimize performance and scalability in every project. I take pride in my responsibility, commitment, and attention to detail, values that I apply to every line of code to ensure high-quality results and meet user needs.",
-  },
-  skills: "Skills",
-  projects: {
-    title: "Projects",
-    proj: [
-      {
-        title: "BotChef",
+    menus: {
+        home: "Home",
+        about: "About",
+        skills: "Skills",
+        studies: "Studies",
+        experience: "Experience",
+        projects: "Projects",
+        contact: "Contact",
+        language: "Language",
+    },
+    welcome: {
+        title: "Hey there,",
+        p1: "My name is Sebastián",
+        p2: "I am a Software Developer",
+        b1: "Get in touch",
+        b2: "Download Resume",
+    },
+    about: {
+        title: "About me",
         description:
-          "Recipe Generator App is a React-based project that uses the Hugging Face API to generate cooking recipes based on a list of ingredients provided by the user. It integrates a backend powered by Node.js and Express to securely manage API tokens.",
-        technologies: "ReactJs, ExpressJs, Bootstrap, Mistral AI, HuggingFace",
-        url: "https://botchef-react.vercel.app/",
-        viewProject: "Link",
-      },
-    ],
-  },
-  education: {
-    title: "Studies and Certifications",
-    degrees: [
-      {
-        title: "Systems Engineering",
-        institution:
-          "Corporación Unificada Nacional de Educación Superior (CUN)",
-        year: "2025-Present",
-      },
-      {
-        title: "Associate Degree in Software Analysis and Development",
-        institution: "SENA",
-        year: "2022-2024",
-      },
-      {
-        title: "Full Stack Web Development Certificate",
-        institution: "Cymetria - TalentoTech",
-        year: "2024",
-      },
-      {
-        title: "Google Cybersecurity Professional Certificate",
-        institution: "Google - Coursera",
-        year: "2024",
-      },
-      {
-        title: "Oracle Next Education - BackEnd",
-        institution: "Oracle - Alura",
-        year: "In course",
-      },
-    ],
-  },
-  experience: {
-    title: "Experience",
-    jobs: [
-      {
-        position: "Intern",
-        company: "Banco Popular S.A.",
-        period: "January 2024 - July 2024",
-        description: [
-          "Bash scripting",
-          "Computers configuration",
-          "VPN appication support",
-          "Tools instalation",
+            "My name is Sebastian and I am a passionate software developer dedicated to building robust and efficient solutions. I have worked with technologies such as Django, Python, JavaScript, React, HTML, and CSS, always striving to optimize performance and scalability in every project. I take pride in my responsibility, commitment, and attention to detail, values that I apply to every line of code to ensure high-quality results and meet user needs.",
+    },
+    skills: "Skills",
+    projects: {
+        title: "Projects",
+        proj: [
+            {
+                title: "BotChef",
+                description:
+                    "Recipe Generator App is a React-based project that uses the Hugging Face API to generate cooking recipes based on a list of ingredients provided by the user. It integrates a backend powered by Node.js and Express to securely manage API tokens.",
+                technologies:
+                    "ReactJs, ExpressJs, Bootstrap, Mistral AI, HuggingFace",
+                url: "https://botchef-react.vercel.app/",
+                viewProject: "Link",
+            },
+            {
+                title: "La Napolitana",
+                description:
+                    "The full stack project for the Napolitana website is a comprehensive application that stores contact form, reservation, and online order data in a database. Additionally, the system automatically sends a confirmation email when data is successfully saved, ensuring a seamless and efficient user experience.",
+                technologies:
+                    "DjangoRestFramework, EmailJs, JavaScript Vanilla, CSS",
+                url: "https://sebg02.github.io/la-napolitana/",
+                viewProject: "Link",
+            },
         ],
-      },
-    ],
-  },
-  contact: {
-    title: "Contact",
-    text: "I am always open to new opportunities and collaborations. If you have any questions, a project in mind, or just want to chat, feel free to reach out. I would be happy to hear from you.",
-  },
+    },
+    education: {
+        title: "Studies and Certifications",
+        degrees: [
+            {
+                title: "Systems Engineering",
+                institution:
+                    "Corporación Unificada Nacional de Educación Superior (CUN)",
+                year: "2025-Present",
+            },
+            {
+                title: "Associate Degree in Software Analysis and Development",
+                institution: "SENA",
+                year: "2022-2024",
+            },
+            {
+                title: "Full Stack Web Development Certificate",
+                institution: "Cymetria - TalentoTech",
+                year: "2024",
+            },
+            {
+                title: "Google Cybersecurity Professional Certificate",
+                institution: "Google - Coursera",
+                year: "2024",
+            },
+            {
+                title: "Oracle Next Education - BackEnd",
+                institution: "Oracle - Alura",
+                year: "In course",
+            },
+        ],
+    },
+    experience: {
+        title: "Experience",
+        jobs: [
+            {
+                position: "Intern",
+                company: "Banco Popular S.A.",
+                period: "January 2024 - July 2024",
+                description: [
+                    "Bash scripting",
+                    "Computers configuration",
+                    "VPN appication support",
+                    "Tools instalation",
+                ],
+            },
+            {
+                position: "Independent developer",
+                company: "Software development services",
+                period: "July 2024 - Present",
+                description: [
+                    "Fullstack proyects creation",
+                    "Front-end development with React",
+                    "Java Programming",
+                    "Back-end development with Django/Spring",
+                ],
+            },
+        ],
+    },
+    contact: {
+        title: "Contact",
+        text: "I am always open to new opportunities and collaborations. If you have any questions, a project in mind, or just want to chat, feel free to reach out. I would be happy to hear from you.",
+    },
 };
--- src/context/spanish.js
+++ src/context/spanish.js
@@ -1,89 +1,110 @@
 export const es = {
-  menus: {
-    home: "Inicio",
-    about: "Acerca de",
-    skills: "Tecnologías",
-    studies: "Estudios",
-    experience: "Experiencia",
-    projects: "Proyectos",
-    contact: "Contacto",
-    language: "Idioma",
-  },
-  welcome: {
-    title: "Hola,",
-    p1: "Me llamo Sebastián",
-    p2: "Soy un Desarrollador de Software",
-    b1: "Contáctame",
-    b2: "Descargar Curriculum",
-  },
-  about: {
-    title: "Acerca de mí",
-    description:
-      "Me llamo Sebastián y soy un desarrollador de software apasionado por crear soluciones robustas y eficientes. He trabajado con tecnologías como Django, Python, JavaScript, React, HTML y CSS, siempre buscando optimizar el rendimiento y la escalabilidad de los proyectos. Me caracterizo por mi responsabilidad, compromiso y atención al detalle, valores que aplico en cada línea de código para garantizar resultados de calidad y satisfacer las necesidades de los usuarios.",
-  },
-  skills: "Tecnologías",
-  projects: {
-    title: "Proyectos",
-    proj: [
-      {
-        title: "BotChef",
+    menus: {
+        home: "Inicio",
+        about: "Acerca de",
+        skills: "Tecnologías",
+        studies: "Estudios",
+        experience: "Experiencia",
+        projects: "Proyectos",
+        contact: "Contacto",
+        language: "Idioma",
+    },
+    welcome: {
+        title: "Hola,",
+        p1: "Me llamo Sebastián",
+        p2: "Soy un Desarrollador de Software",
+        b1: "Contáctame",
+        b2: "Descargar Curriculum",
+    },
+    about: {
+        title: "Acerca de mí",
         description:
-          "La app de generación de recetas es un proyecto basado en React que utiliza la API de Hugging Face para generar recetas de cocina a partir de una lista de ingredientes proporcionada por el usuario. Integra un backend impulsado por Node.js y Express para gestionar de manera segura los tokens de la API.",
-        technologies: "ReactJs, ExpressJs, Bootstrap, Mistral AI, HuggingFace",
-        url: "https://botchef-react.vercel.app/",
-        viewProject: "Link",
-      },
-    ],
-  },
-  education: {
-    title: "Estudios y certificaciones  ",
-    degrees: [
-      {
-        title: "Ingeniería de Sistemas",
-        institution:
-          "Corporación Unificada Nacional de Educación Superior (CUN)",
-        year: "2025-Actualidad",
-      },
-      {
-        title: "Tecnólogo en Análisis y Desarrollo de Software",
-        institution: "SENA",
-        year: "2022-2024",
-      },
-      {
-        title: "Certificación en Desarrollo Web Full Stack",
-        institution: "Cymetria - TalentoTech",
-        year: "2024",
-      },
-      {
-        title: "Certificado profesional de Ciberseguridad de Google",
-        institution: "Google - Coursera",
-        year: "2024",
-      },
-      {
-        title: "Oracle Next Education - BackEnd",
-        institution: "Oracle - Alura",
-        year: "En curso",
-      },
-    ],
-  },
-  experience: {
-    title: "Experiencia Profesional",
-    jobs: [
-      {
-        position: "Aprendiz Sena",
-        company: "Banco Popular S.A.",
-        period: "Enero 2024 - Julio 2024",
-        description: [
-          "Creación de scripts en bash",
-          "Configuración de equipos",
-          "Soporte aplicación de VPN",
-          "Instalación de herramientas",
+            "Me llamo Sebastián y soy un desarrollador de software apasionado por crear soluciones robustas y eficientes. He trabajado con tecnologías como Django, Python, JavaScript, React, HTML y CSS, siempre buscando optimizar el rendimiento y la escalabilidad de los proyectos. Me caracterizo por mi responsabilidad, compromiso y atención al detalle, valores que aplico en cada línea de código para garantizar resultados de calidad y satisfacer las necesidades de los usuarios.",
+    },
+    skills: "Tecnologías",
+    projects: {
+        title: "Proyectos",
+        proj: [
+            {
+                title: "BotChef",
+                description:
+                    "La app de generación de recetas es un proyecto basado en React que utiliza la API de Hugging Face para generar recetas de cocina a partir de una lista de ingredientes proporcionada por el usuario. Integra un backend impulsado por Node.js y Express para gestionar de manera segura los tokens de la API.",
+                technologies:
+                    "ReactJs, ExpressJs, Bootstrap, Mistral AI, HuggingFace",
+                url: "https://botchef-react.vercel.app/",
+                viewProject: "Link",
+            },
+            {
+                title: "La Napolitana",
+                description:
+                    "El proyecto full stack para la página web de la Napolitana es una aplicación integral que almacena los datos de los formularios de contacto, reservaciones y pedidos en línea en una base de datos. Además, el sistema envía automáticamente un correo electrónico de confirmación cuando los datos se guardan correctamente, garantizando una experiencia fluida y eficiente para los usuarios.",
+                technologies:
+                    "DjangoRestFramework, EmailJs, JavaScript Vanilla, CSS",
+                url: "https://sebg02.github.io/la-napolitana/",
+                viewProject: "Link",
+            },
         ],
-      },
-    ],
-  },
-  contact: {
-    title: "Contacto",
-    text: "Estoy siempre abierto a nuevas oportunidades y colaboraciones. Si tienes alguna pregunta, proyecto en mente o simplemente quieres charlar, no dudes en contactarme. Estaré encantado de responderte.",
-  },
+    },
+    education: {
+        title: "Estudios y certificaciones  ",
+        degrees: [
+            {
+                title: "Ingeniería de Sistemas",
+                institution:
+                    "Corporación Unificada Nacional de Educación Superior (CUN)",
+                year: "2025-Actualidad",
+            },
+            {
+                title: "Tecnólogo en Análisis y Desarrollo de Software",
+                institution: "SENA",
+                year: "2022-2024",
+            },
+            {
+                title: "Certificación en Desarrollo Web Full Stack",
+                institution: "Cymetria - TalentoTech",
+                year: "2024",
+            },
+            {
+                title: "Certificado profesional de Ciberseguridad de Google",
+                institution: "Google - Coursera",
+                year: "2024",
+            },
+            {
+                title: "Oracle Next Education - BackEnd",
+                institution: "Oracle - Alura",
+                year: "En curso",
+            },
+        ],
+    },
+    experience: {
+        title: "Experiencia Profesional",
+        jobs: [
+            {
+                position: "Aprendiz Sena",
+                company: "Banco Popular S.A.",
+                period: "Enero 2024 - Julio 2024",
+                description: [
+                    "Creación de scripts en bash",
+                    "Configuración de equipos",
+                    "Soporte aplicación de VPN",
+                    "Instalación de herramientas",
+                ],
+            },
+            {
+                position: "Desarrollador independiente",
+                company: "Servicios de desarrollo de software ",
+                period: "Julio 2024 - Actualidad",
+                description: [
+                    "Creación de proyectos fullstack",
+                    "Desarrollo Front-end con React/JavaScript",
+                    "Programación en Java",
+                    "Desarrollo Back-end con Django/Spring",
+                ],
+            },
+        ],
+    },
+    contact: {
+        title: "Contacto",
+        text: "Estoy siempre abierto a nuevas oportunidades y colaboraciones. Si tienes alguna pregunta, proyecto en mente o simplemente quieres charlar, no dudes en contactarme. Estaré encantado de responderte.",
+    },
 };
